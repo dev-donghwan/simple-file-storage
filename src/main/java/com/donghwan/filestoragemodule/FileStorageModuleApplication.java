31e9e2d71bce7b91249b05a354bcc76a1ab8dda9
dev-donghwan <ycdoit@naver.com>
2024-10-15 17:34:08 +0900

    initial commit

---
+++ src/main/java/com/donghwan/filestoragemodule/FileStorageModuleApplication.java
@@ -0,0 +1,13 @@
+package com.donghwan.filestoragemodule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileStorageModuleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FileStorageModuleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/donghwan/filestoragemodule/FileStorageModuleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.donghwan.filestoragemodule;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileStorageModuleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
